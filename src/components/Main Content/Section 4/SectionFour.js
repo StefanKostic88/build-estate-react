9b8d1a54ac81d16d4b8dabdb8d0bcf56cbb874f5
stefanKostic88 <stefankostic88@yahoo.com>
2022-11-12 12:45:45 +0100

    Section 4 completed

--- src/components/Main Content/Section 4/SectionFour.js
+++ src/components/Main Content/Section 4/SectionFour.js
@@ -1,6 +1,7 @@
 import SectionTitle from "../../UI/SectionCard/SectionTitle/SectionTitle.js";
 import SectionCard from "../../UI/SectionCard/SectionCard.js";
 import SectionContent from "../../UI/SectionCard/SectionContent/SectionContent.js";
+import Button from "../../UI/Button/Button.js";
 
 import classes from "./SectionFour.module.css";
 
@@ -8,7 +9,19 @@ const SectionFour = ({ title }) => {
   return (
     <SectionCard className="">
       <SectionTitle sectionTitle={title} />
-      <SectionContent></SectionContent>
+      <SectionContent>
+        <form className={classes.action}>
+          <div className={classes["actions-container"]}>
+            <input type="text" placeholder="Name" />
+            <input type="email" placeholder="Email" />
+            <input type="text" placeholder="Telephone" />
+          </div>
+          <textarea placeholder="Message"></textarea>
+          <Button className="" type="submit">
+            Submit
+          </Button>
+        </form>
+      </SectionContent>
     </SectionCard>
   );
 };
